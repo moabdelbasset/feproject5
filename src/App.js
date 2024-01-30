d00ead1b79f9e4b9e738eb93897c9565fa99f5fe
Mohamed Ayman <59439627+moabdelbasset@users.noreply.github.com>
2024-01-30 08:06:24 +0000

    adding footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 import styles from './App.module.css';
+
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from '../styles/Footer.module.css';
+
+const Footer = () => {
+  return (
+    <div className={styles.Footer}>
+      <div>
+        Developed by <strong>Mohamed Ayman</strong>
+      </div>
+      <div className={styles.Icons}>
+        <a href="https://github.com/moabdelbasset" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-github"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/mohamed-ayman-b29b5b66/" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-linkedin-in"></i>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
