ba951cb11fa402d460e98bb143a62416d62113c9
Mohamed Ayman <59439627+moabdelbasset@users.noreply.github.com>
2024-01-30 20:01:34 +0000

    useContext hook to change the navbar based on user status

--- src/App.js
+++ src/App.js
@@ -6,21 +6,45 @@ import {Route, Switch} from 'react-router-dom';
 import './api/axiosDefaults'
 import SignUpForm from './pages/auth/SignUpForm';
 import SignInForm from './pages/auth/SignInForm';
+import { createContext, useEffect, useState } from 'react';
+import axios from 'axios';
+export const CurrentUserContext = createContext();
+export const SetCurrentUsetContext = createContext();
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null)
+
+  const handleMount = async () => {
+    try {
+      const {data} = await axios.get('dj-resxt-auth/user/')
+      setCurrentUser(data)
+    } catch(err) {
+      console.log(err)
+    }
+
+  }
+
+  useEffect(() => {
+    handleMount()
+  }, [])
+
   return (
-    <div className={styles.App}>
-      <NavBar />
-      <Container className={styles.Main}>
-        <Switch>
-          <Route exact path="/" render={()=> <h1>Home page</h1>} />
-          <Route exact path="/signin" render={()=> <SignInForm />} />
-          <Route exact path="/signup" render={()=> <SignUpForm />} />
-          <Route render={()=><p>Page not found!</p>} />
-        </Switch>
-      </Container>
-      <Footer />
-    </div>
+    <CurrentUserContext.Provider value={currentUser}>
+      <SetCurrentUsetContext.Provider value={setCurrentUser}>
+        <div className={styles.App}>
+          <NavBar />
+          <Container className={styles.Main}>
+            <Switch>
+              <Route exact path="/" render={()=> <h1>Home page</h1>} />
+              <Route exact path="/signin" render={()=> <SignInForm />} />
+              <Route exact path="/signup" render={()=> <SignUpForm />} />
+              <Route render={()=><p>Page not found!</p>} />
+            </Switch>
+          </Container>
+          <Footer />
+        </div>
+      </SetCurrentUsetContext.Provider>
+    </CurrentUserContext.Provider>
   );
 }
 
--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useContext, useState } from "react";
 import axios from "axios";
 
 import Form from "react-bootstrap/Form";
@@ -14,8 +14,12 @@ import { Link, useHistory } from "react-router-dom";
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
+import { SetCurrentUsetContext } from "../../App";
 
 function SignInForm() {
+
+  const setCurrentUser = useContext(SetCurrentUsetContext)
+
   const [signInData, setSignInData] = useState({
     username: "",
     password: "",
@@ -28,7 +32,8 @@ function SignInForm() {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      await axios.post("/dj-rest-auth/login/", signInData);
+      const {data} = await axios.post("/dj-rest-auth/login/", signInData);
+      setCurrentUser(data.user)
       history.push("/");
     } catch (err) {
       setErrors(err.response?.data);
