e25594a005509dbd915bf5f3b2a0e3ce55bd9bad
Mohamed Ayman <59439627+moabdelbasset@users.noreply.github.com>
2024-01-30 06:41:10 +0000

    creating navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import Button from 'react-bootstrap/Button'
+import NavBar from './components/NavBar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Button variant="primary">Primary</Button>
+      <NavBar />
     </div>
   );
 }
